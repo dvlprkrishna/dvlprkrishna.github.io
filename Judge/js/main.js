b00349c02cfc440f411d888118bab704811664e1
kripsahu <krishna4721995@gmail.com>
2017-10-14 23:10:59 +0530

    added Judge temp

---
+++ Judge/js/main.js
@@ -0,0 +1,33 @@
+(function() {
+
+    var quotes = $(".ct-quote");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+
+    showNextQuote();
+
+})();
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+        $('a.backtotop').fadeIn('slow');
+    } else {
+        $('a.backtotop').fadeOut('slow');
+    }
+});
+
+$('a.backtotop').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700);
+    return false;
+});
