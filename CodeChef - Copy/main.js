52e8a238fc5b7382f15faabe227ebe88f4708f04
Krishna Sahu <472krishna@gmail.com>
2019-05-08 18:47:33 +0530

    Add files via upload

---
+++ CodeChef - Copy/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+	//var page1;
+	/*$("#submit1").click(function (){
+		var x = $(":text").val();
+		if(x !== ""){
+			// console.log("not empty");
+	  		localStorage.setItem("key1", $("#fname").val());
+	  		localStorage.setItem("key2", $("#sname").val());
+	  		// console.log(localStorage);
+	  		var url = "page2.html";
+			$(location).attr('href',url);
+		} else {
+			console.log("err")
+		}
+	});*/
+
+	// page 2
+	/*$("#submit2").click(function (){
+		var x = $(":text").val();
+		if(x !== ""){
+			// console.log("not empty");
+	  		localStorage.setItem("key3", $("#age").val());
+	  		localStorage.setItem("key4", $("#blood").val());
+	  		//console.log(localStorage);
+	  		var url = "result.html";
+			$(location).attr('href',url);
+		} else {
+			console.log("err")
+		}
+	});*/
+
+	// Results
+	/*var currentURL = window.location.href;
+	var regexTest = new RegExp('result.html');
+	console.log(regexTest.test(currentURL));
+
+
+	if(regexTest.test(currentURL)){
+		console.log("got it");
+		$(document).ready(function(){
+			for(var i=0; i<=3; i++){
+				console.log(i);
+				console.log(localStorage.getItem("key`${i}`"));
+			};
+		});
+	} else {
+		console.log("err");
+	}*/
+
+	/*$(document).ready(function(){
+		for(var i=0; i<=3; i++){
+			console.log(i);
+			console.log(localStorage.getItem("key`${i}`"));
+		};
+	});*/
+});
